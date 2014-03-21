9d954e1c7773545ae753916b0030c6748887a9ec
muddmaker <djshredd@gmail.com>
2014-03-21 13:48:47 -0700

    Add serialization functions
    meant for converting scores into and out of strings

--- HuntTheWumpus/HuntTheWumpus/ScoreHandler.cs
+++ HuntTheWumpus/HuntTheWumpus/ScoreHandler.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Xml;
+using System.IO;
 
 namespace HuntTheWumpus
 {
@@ -17,12 +17,12 @@ namespace HuntTheWumpus
         /// <summary>
         /// Represents a single score
         /// </summary>
-        public class Score : XmlNode, IComparable
+        public class Score
         {
             /// <summary>
             /// Name of the user
             /// </summary>
-            public string name = "";
+            public string Name = "";
 
             /// <summary>
             /// Points recieved from questions/money
@@ -38,10 +38,81 @@ namespace HuntTheWumpus
             /// Seconds taken during the game
             /// </summary>
             public ulong Time = 0;
+
+            /// <summary>
+            /// Basic constructor
+            /// </summary>
+            public Score()
+            { }
+
+            /// <summary>
+            /// Basic constructor for giving all objects
+            /// </summary>
+            /// <param name="name">Name of the user</param>
+            /// <param name="points">Points recieved from questions/money</param>
+            /// <param name="turns">Number of turns the user has taken</param>
+            /// <param name="time">Seconds taken during the game</param>
+            public Score(string name, ulong points, ulong turns, ulong time)
+            {
+                Name = name;
+                Points = points;
+                Turns = turns;
+                Time = time;
+            }
+
+            /// <summary>
+            /// Serialization into a unit that will go into the file
+            /// </summary>
+            /// <returns>The object as a string</returns>
+            public string Serialize()
+            {
+                return Name + ';' + Convert.ToString(Points) + ':' + Convert.ToString(Turns) + '|' + Convert.ToString(Time) + '\n';
+            }
         }
 
         public ScoreHandler(Score score)
         {
+            HighScores.Add(score);
+        }
+
+        /// <summary>
+        /// Takes the contents of a file and converts it into highscores
+        /// </summary>
+        /// <param name="serial">Contents of the file</param>
+        void Deserialize(string serial)
+        {
+            string temp = "";
+            Score score = new Score();
+
+            for (int i = 0; i < serial.Length; i++)
+            {
+                if (serial[i] == ';')
+                {
+                    score.Name = temp;
+                    temp = "";
+                }
+                else if (serial[i] == ':')
+                {
+                    ulong.TryParse(temp, out score.Points);
+                    temp = "";
+                }
+                else if (serial[i] == '|')
+                {
+                    ulong.TryParse(temp, out score.Turns);
+                    temp = "";
+                }
+                else if (serial[i] == '\n')
+                {
+                    ulong.TryParse(temp, out score.Time);
+                    temp = "";
+                    HighScores.Add(score);
+                    score = new Score();
+                }
+                else
+                {
+                    temp += serial[i];
+                }
+            }
         }
     }
 }
