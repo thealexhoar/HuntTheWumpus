c9a242cfc518a6dbd953aa0da8d5417f43bc11b8
muddmaker <djshredd@gmail.com>
2014-03-26 08:07:33 -0700

    Start working on networked score storage
    This will be used for global leaderboards

--- HuntTheWumpus/HuntTheWumpus/ScoreHandler.cs
+++ HuntTheWumpus/HuntTheWumpus/ScoreHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Net;
 using System.IO;
 
 namespace HuntTheWumpus
---
+++ ScoreServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScoreServer
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
