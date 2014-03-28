f892ead79ec070420375f618545575e3c8d2c441
muddmaker <djshredd@gmail.com>
2014-03-27 22:01:54 -0700

    Bring over complete score class, fix formatting
    oh, how much i love moving between ides, and their different means of
    handling tabs/spaces

--- ScoreServer/Program.cs
+++ ScoreServer/Program.cs
@@ -5,30 +5,30 @@ using System.Threading;
 
 namespace ScoreServer
 {
-	/// <summary>
-	/// Contains the server for storing high scores
-	/// </summary>
+    /// <summary>
+    /// Contains the server for storing high scores
+    /// </summary>
     class Program
     {
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
         static void Main()
         {
         }
 
-		/// <summary>
-		/// Receives a request from a client
-		/// </summary>
-		static void ReceiveRequest()
-		{
-		}
+        /// <summary>
+        /// Receives a request from a client
+        /// </summary>
+        static void ReceiveRequest()
+        {
+        }
 
-		/// <summary>
-		/// Sends the top ten highest scores for the client to use.
-		/// </summary>
-		static void SendTopTen()
-		{
-		}
+        /// <summary>
+        /// Sends the top ten highest scores for the client to use.
+        /// </summary>
+        static void SendTopTen()
+        {
+        }
     }
 }
--- ScoreServer/Score.cs
+++ ScoreServer/Score.cs
@@ -2,17 +2,83 @@
 
 namespace ScoreServer
 {
-	/// <summary>
-	/// Represents a score in memory
-	/// </summary>
-	public class Score
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ScoreServer.Score"/> class.
-		/// </summary>
-		public Score()
-		{
-		}
-	}
+    /// <summary>
+    /// Represents a score in memory
+    /// </summary>
+    public class Score : IComparable<Score>
+    {
+        /// <summary>
+        /// Name of the user
+        /// </summary>
+        public string Name = "";
+
+        /// <summary>
+        /// Points recieved from questions/money
+        /// </summary>
+        public ulong Points = 0;
+
+        /// <summary>
+        /// Number of turns the user has taken
+        /// </summary>
+        public ulong Turns = 0;
+
+        /// <summary>
+        /// Seconds taken during the game
+        /// </summary>
+        public ulong Time = 0;
+
+        /// <summary>
+        /// Basic constructor
+        /// </summary>
+        public Score()
+        { }
+
+        /// <summary>
+        /// Basic constructor for giving all objects
+        /// </summary>
+        /// <param name="name">Name of the user</param>
+        /// <param name="points">Points recieved from questions/money</param>
+        /// <param name="turns">Number of turns the user has taken</param>
+        /// <param name="time">Seconds taken during the game</param>
+        public Score(string name, ulong points, ulong turns, ulong time)
+        {
+            Name = name;
+            Points = points;
+            Turns = turns;
+            Time = time;
+        }
+
+        /// <summary>
+        /// Serialization into a unit that will go into the file
+        /// </summary>
+        /// <returns>The object as a string</returns>
+        public string Serialize()
+        {
+            return Name + ';' + Convert.ToString(Points) + ':' + Convert.ToString(Turns) + '|' + Convert.ToString(Time) + '\n';
+        }
+
+        /// <summary>
+        /// Allows comparing to take place between different scores
+        /// </summary>
+        /// <param name="obj">The other score</param>
+        /// <returns>Represents whether equal to, greater than, or less than</returns>
+        public int CompareTo(Score obj)
+        {
+            if (Points > obj.Points)
+                return -1;
+            else if (Points < obj.Points)
+                return 1;
+            else if (Time < obj.Time)
+                return -1;
+            else if (Time > obj.Time)
+                return 1;
+            else if (Turns < obj.Turns)
+                return -1;
+            else if (Turns > obj.Turns)
+                return 1;
+            else
+                return 0;
+        }
+    }
 }
 
